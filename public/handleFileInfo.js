9ac06aadbabba08459022f6099e568faa63654aa
igloude <igloude@gmail.com>
2023-11-19 09:31:57 -0600

    added event handler for image upload

---
+++ public/handleFileInfo.js
@@ -0,0 +1,9 @@
+document.getElementById('imageInput').addEventListener('change', function (e) {
+  console.log(e.target.files[0]);
+  // TODO: return image data: filename, extension, and file size
+  //    e.target.files[0]
+
+  // TODO: return resolution using multer
+
+  // TODO: validate file size
+});
